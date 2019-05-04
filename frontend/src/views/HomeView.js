12fab9f3249e4e54520cbca37101914655214896
Brian Craig <briancraigok@gmail.com>
2019-05-04 00:40:02 -0300

    homeview

--- frontend/src/views/HomeView.js
+++ frontend/src/views/HomeView.js
@@ -3,14 +3,14 @@ import Candidatas from '../components/Candidatas';
 import HomeTopSlider from '../components/HomeTopSlider';
 import GanadorasAnteriores from '../components/GanadorasAnteriores';
 import { H1, H2 } from '@blueprintjs/core';
+import HomeLinks from '../components/HomeLinks';
 
 const HomeView = () => 
     <div>
         <H1>Grandes Mujeres</H1>        
         <HomeTopSlider />
         <H2>Destacadas del mes</H2>
-        <GanadorasAnteriores />
-        <Candidatas />
+        <HomeLinks />
     </div>
 
 export default HomeView;
