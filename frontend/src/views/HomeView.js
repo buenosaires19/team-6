cc9af2d44dfbe61449c104f24553f9de6d23be06
Brian Craig <briancraigok@gmail.com>
2019-05-04 00:41:56 -0300

    home view 2

--- frontend/src/views/HomeView.js
+++ frontend/src/views/HomeView.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Candidatas from '../components/Candidatas';
 import HomeTopSlider from '../components/HomeTopSlider';
-import GanadorasAnteriores from '../components/GanadorasAnteriores';
 import { H1, H2 } from '@blueprintjs/core';
 import HomeLinks from '../components/HomeLinks';
 
