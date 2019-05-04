306eefe73956063bb462e9f16b915ceefd7c345b
Barbara Craig <craig.s.barbara@gmail.com>
2019-05-04 05:13:36 -0300

    headerchanges css

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,16 +3,19 @@ import './Header.css';
 import { Button, Alignment, Navbar } from "@blueprintjs/core"; 
 import { ALIGNMENT_RIGHT } from '@blueprintjs/icons/lib/esm/generated/iconContents';
 
+
 const Header = () => 
   <Navbar className="header">
    
         <Navbar.Group align={Alignment.LEFT}> 
-            <Navbar.Heading className="bp3-nav">Chicas en Tecnología</Navbar.Heading>    
+            <Navbar.Heading className="bp3-nav">CHICAS EN TECNLOGÍA</Navbar.Heading> 
+            <span class="bp3-navbar-divider"></span>   
         </Navbar.Group>
 
         <Navbar.Group align={Alignment.RIGHT}>
             <Navbar.Divider />
             <Button className="bp3-minimal" icon="home" text="Ingresar" />
+            <span class="bp3-navbar-divider"></span>   
             <Button className="bp3-minimal" icon="document" text="Postular" />
         </Navbar.Group>
   </Navbar> 
