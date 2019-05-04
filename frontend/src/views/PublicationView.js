31942c78e88085a67c53660de0d06f6dbfc2c3f2
Barbara Craig <craig.s.barbara@gmail.com>
2019-05-04 02:13:49 -0300

    ajustes

--- frontend/src/views/PublicationView.js
+++ frontend/src/views/PublicationView.js
@@ -5,12 +5,15 @@ import { Button,ButtonGroup, AnchorButton, Card, Elevation, Icon } from "@bluepr
 
 
 
+
 const PublicationView = () => 
     <p>
         <h1 className="titulo">Candidatas</h1>
         
-        <Card interactive={true} elevation={Elevation.TWO}>
+         <Card interactive={true} elevation={Elevation.TWO}>
+         
         <p className="nombreCandidata">Maria Salerno</p>
+        <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSqfYIcPYstFZS0tdw24sju588C5O-EksHFpK8PlqedEnNQvaajQ"} alt="Logo" />
         <p className= "especialidad">Bioquímica</p>
         <p className= "actividad">Investigadora del CONICET </p>
         <p className= "detalles">Egresada de la UBA en el 2009 de Lic. en Bioquímica. Actualmente se encarga de la parte de investigaciń y desarrollo en el laboratorio de enfermedades virales de ...</p>
