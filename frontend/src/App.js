526d5b8997f3bd11da078d386fbc0b20b76fb920
Brian Craig <briancraigok@gmail.com>
2019-05-03 21:03:42 -0300

    added component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import postulacion from './mock/postulacion.json';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Header />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => 
+    <div>
+        <p>Cabecera</p>
+    </div>
+
+export default Header;
