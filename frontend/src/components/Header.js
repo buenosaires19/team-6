9eeee6d2d735a0146662957a0a74adb732294181
Barbara Craig <craig.s.barbara@gmail.com>
2019-05-04 09:30:42 -0300

    .

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,19 +11,21 @@ const Header = () =>
             <Navbar.Heading className="bp3-nav"><img src={logo} style={{height:36}}/></Navbar.Heading>
             <Navbar.Heading className="bp3-nav">GRANDES MUJERES</Navbar.Heading>
             <span class="bp3-navbar-divider"></span>
+            <Navbar.Heading className="bp3-nav"><Link to={`/`}>HOME</Link></Navbar.Heading>
+            <span class="bp3-navbar-divider"></span>
             <Navbar.Heading className="bp3-nav"><Link to={`/buscar`}>GANADORAS</Link></Navbar.Heading>
             <span class="bp3-navbar-divider"></span>
             <Navbar.Heading className="bp3-nav"><Link to={`/buscar`}>CANDIDATAS</Link></Navbar.Heading>
             <span class="bp3-navbar-divider"></span>
-            <Navbar.Heading className="bp3-nav"><Link to={`/`}>HOME</Link></Navbar.Heading>
-            <span class="bp3-navbar-divider"></span>
+            
+            
         </Navbar.Group>
 
         <Navbar.Group align={Alignment.RIGHT}>
             <Navbar.Divider />
             <Button className="bp3-minimal" icon="home" text="Ingresar" />
             <span class="bp3-navbar-divider"></span>
-            <Link to={`/postularse`}><Button className="bp3-minimal" icon="document" text="Postular" /></Link>
+            <Link to={`/postularse`}><Button className="bp3-minimal" icon="document" text="Postular" /></Link>            
         </Navbar.Group>
   </Navbar>
 
